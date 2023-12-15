5f9d736aa37021a842b9348641a7b60d7ab7d1b0
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2023-12-15 07:58:32 -0500

    Add case statement and nullifs sql file

---
+++ case statment and nullifs.sql
@@ -0,0 +1,27 @@
+/**
+* Database: Store
+* Table: products
+* Create a case statement that's named "price class" where if a product is over 20 dollars you show 'expensive'
+* if it's between 10 and 20 you show 'average' 
+* and of is lower than or equal to 10 you show 'cheap'.
+*/
+select title,
+	   price,
+	   case when price > 20 then 'expensive'
+	   		when price between 10 and 20 then 'average'
+			else 'cheap'
+	   end as price_class
+from products;
+
+
+/*
+* DB: Store
+* Table: products
+* Question: Show NULL when the product is not on special (0)
+*/
+select prod_id,
+	   title,
+	   price,
+	   special,
+	   nullif(special, 0) as not_on_special
+from products;
