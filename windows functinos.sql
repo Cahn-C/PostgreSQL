b8539eefb3b42fb7ff6566b89516ba0280164c28
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2023-12-19 23:22:24 -0500

    Update windows functinos.sql

--- windows functinos.sql
+++ windows functinos.sql
@@ -16,12 +16,12 @@ from country;
 *  Table: Country
 */
 select distinct continent, 
-	   sum(population) over(partition by continent) as total_population_per_continent,
-	   -- Divide the total sum of the population per continent by the total sum of the population
-	   -- Used the roud() function to round it to the nearest number
-	   -- Used the concat() function to add the percentage sign at the end
-	   concat(round(sum(population::float4) over(partition by continent) /
-	   sum(population::float4) over() * 100), '%') as population_percentage
+       sum(population) over(partition by continent) as total_population_per_continent,
+       -- Divide the total sum of the population per continent by the total sum of the population
+       -- Used the roud() function to round it to the nearest number
+       -- Used the concat() function to add the percentage sign at the end
+       concat(round(sum(population::float4) over(partition by continent) /
+       sum(population::float4) over() * 100), '%') as population_percentage
 from country;
 
 
@@ -32,9 +32,9 @@ from country;
 *  Table: Regions (Join + Window function)
 */
 select distinct r.id, 
-	   r."name", 
-	   count(t.id) over(partition by r.id order by r."name") as number_of_towns_per_region
+       r."name", 
+       count(t.id) over(partition by r.id order by r."name") as number_of_towns_per_region
 from regions r 
 inner join departments d on r.code = d.region
-inner join towns t 		 on d.code = t.department
+inner join towns t       on d.code = t.department
 order by r.id;
